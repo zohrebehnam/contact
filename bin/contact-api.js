d6bc1b0355f84c52de5e71573345498dee41271e
Zohreh Behnampoor <behnampoorz@gmail.com>
2021-06-03 17:31:58 +0430

    Apply all methods of crud on contact api.

--- bin/contact-api.js
+++ bin/contact-api.js
@@ -4,7 +4,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoDriver from './mongo.js';
-import ObjectId from 'mongodb';
+import mongodb from 'mongodb';
 
 const app = express();
 const port = 3000;
@@ -16,26 +16,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/contact',async (req, res) => {
+app.get('/contact', async (req, res) => {
     
-    var contacts = [];
-    contacts = await mongoDriver.database.collection("contact").find({}).toArray();
-    res.send(contacts);
+    let contacts = await mongoDriver.database.collection("contact").find({}).toArray();
+    res.status(contacts.length ? 200 : 204).send(contacts);
 });
 
-app.get('/contact/:id',async (req, res) => {
+app.get('/contact/:id', async (req, res) => {
 
-    console.log(req.params.id);
-    let contact = await mongoDriver.database.collection("contact").findOne({_id: new ObjectId(req.params.id)});
-    res.send(contact);
-});
-
-app.delete('/contact/:id',async (req, res) => {
-    
-    let query = await mongoDriver.database.collection("contact").remove({_id : req.params.id});
-    if (query.result.ok) {
-        res.send('deleted');
+    try {
+        let contact = await mongoDriver.database.collection("contact").findOne({_id: mongodb.ObjectId(req.params.id)});
+        if (contact !== null) {
+            return res.status(200).send(contact);
+        }
     }
+    catch(error) {
+        console.error(error.message);
+    }
+
+    return res.status(204).send({});
+});
+
+app.delete('/contact/:id', async (req, res) => {
+
+    let query = await mongoDriver.database.collection("contact").deleteOne({_id: mongodb.ObjectId(req.params.id)});
+    if (query.deletedCount) {
+        return res.send({status: true, message:'Deleted'});
+    }
+
+    return res.send({status: false, message: 'Delete failed'});
+});
+
+app.post('/post', async (req, res) => {
+
+    let contact = {
+        name: req.body.name,
+        mobile: req.body.mobile,
+        email: req.body.email
+    };
+    let query = await mongoDriver.database.collection("contact").insert(contact);
+    if (query.result.ok) {
+        return res.send({status: true, message:'Saved new contact'});
+    }
+
+    return res.send({status: false, message: 'Save failed'});
+});
+
+app.put('/update/:id', async (req, res) => {
+
+    let query = await mongoDriver.database.collection("contact").updateOne({_id: mongodb.ObjectId(req.params.id)}, { $set: req.body });
+    if (query.result.ok) {
+        return res.send({status: true, message: 'Updated contact'});
+    }
+
+    return res.send({status: false, message: 'Update failed'});
 });
 
 app.listen(port, () => console.log(`Contact app listening on port ${port}!`))
